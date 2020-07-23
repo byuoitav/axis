e680a0b1b8acf0550e725199168928af16a90deb
Daniel Randall <dgrandall@me.com>
2020-07-22 19:47:05 -0600

    change stream to get mjpeg from the camera

--- P5414-E.go
+++ P5414-E.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"image"
 	_ "image/jpeg"
+	"mime"
+	"mime/multipart"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -12,7 +14,8 @@ import (
 )
 
 type P5414E struct {
-	Address string
+	Address            string
+	StreamMaxFrequency time.Duration
 }
 
 const (
@@ -25,6 +28,8 @@ const (
 	_p5414ESnapshotEndpoint = "/axis-cgi/jpg/image.cgi"
 	_p5414ESnapshotWidth    = 640
 	_p5414ESnapshotHeight   = 360
+
+	_p5414EStreamEndpoint = "/mjpg/video.mjpg"
 )
 
 func (c *P5414E) TiltUp(ctx context.Context) error {
@@ -100,33 +105,65 @@ func (c *P5414E) do(ctx context.Context, values url.Values) error {
 }
 
 func (c *P5414E) Stream(ctx context.Context) (chan image.Image, chan error, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("http://%s%s", c.Address, _p5414EStreamEndpoint), nil)
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to build request: %w", err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to make request: %w", err)
+	}
+
+	_, params, err := mime.ParseMediaType(resp.Header.Get("Content-Type"))
+	if err != nil {
+		resp.Body.Close()
+		return nil, nil, fmt.Errorf("unable to parse content-type: %w", err)
+	}
+
 	images := make(chan image.Image)
 	errs := make(chan error)
 
 	go func() {
-		ticker := time.NewTicker(125 * time.Millisecond)
-		defer ticker.Stop()
+		defer resp.Body.Close()
+
+		lastImage := time.Time{}
+		reader := multipart.NewReader(resp.Body, params["boundary"])
 
 		for {
 			select {
-			case <-ticker.C:
-				image, err := c.getSnapshot(ctx)
+			case <-ctx.Done():
+				return
+			default:
+				part, err := reader.NextPart()
 				if err != nil {
-					errs <- err
+					errs <- fmt.Errorf("unable to read next frame: %w", err)
+					continue
+				}
+
+				// if this part came before we wanted another one, just drop it
+				if time.Since(lastImage) <= c.StreamMaxFrequency {
+					continue
+				}
+
+				lastImage = time.Now()
+
+				image, _, err := image.Decode(part)
+				if err != nil {
+					errs <- fmt.Errorf("unable to decode image: %w", err)
 					continue
 				}
 
 				images <- image
-			case <-ctx.Done():
-				return
 			}
 		}
+
 	}()
 
 	return images, errs, nil
 }
 
-func (c *P5414E) getSnapshot(ctx context.Context) (image.Image, error) {
+func (c *P5414E) Snapshot(ctx context.Context) (image.Image, error) {
 	ctx, cancel := context.WithTimeout(ctx, 750*time.Millisecond)
 	defer cancel()
 
